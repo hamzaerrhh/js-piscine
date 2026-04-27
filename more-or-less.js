6e25da4dd226c1d47b509e545834a706dcf2e06c
herrabba <hmzaeer@gmail.com>
2026-04-27 11:07:41 +0100

    p

--- more-or-less.js
+++ more-or-less.js
@@ -1,21 +1,26 @@
  const more =function(arg) {
-    n = parseFloat(arg)
-    return ++n
+    n = parseInt(arg)
+    n=n+1
+    return n
 }
 
 const less =function (arg){
-    n= parseFloat(arg)
-    return --n
+    n= parseInt(arg)
+    n=n-1
+    return n
 }
 
 const add = function (arg1,arg2){
-    n1= parseFloat(arg1)
-    n2= parseFloat(arg2)
-    return n1+n2
+    n1= parseInt(arg1)
+    n2= parseInt(arg2)
+  const  sol=n1+n2
+    return sol
 }
 
 const sub =function (arg1,arg2){
-    n1= parseFloat(arg1)
-    n2= parseFloat(arg2)
-    return n1-n2
+    n1= parseInt(arg1)
+    n2= parseInt(arg2)
+    const sol=n1-n2
+    return sol
 }
+console.log(more(5),typeof more(5));
