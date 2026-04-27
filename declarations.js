d3d6f66510f77b654c39450f26cfe6c255387735
herrabba <hmzaeer@gmail.com>
2026-04-27 10:12:02 +0100

    advance declaration:obj,special char,nested

---
+++ declarations.js
@@ -0,0 +1,18 @@
+const escapeStr="`, \, /, \" and '."
+const arr =[4,'2']
+const obj={
+ str :"1",
+ num : 14,
+ bool :true,
+ undef : undefined
+
+}
+const nested={
+    arr,
+    obj
+}
+
+// console.log(escapeStr)
+// console.log(arr)
+// console.log(obj)
+// console.log(nested)
