8b0b930d25c1436c12018c90526e145383996d74
herrabba <hmzaeer@gmail.com>
2026-04-27 10:56:57 +0100

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,23 @@
+ function more(arg) {
+n= parseFloat(arg)
+return n++
+
+}
+function less(arg){
+n= parseFloat(arg)
+return n--
+}
+function add(arg1,arg2){
+    n1= parseFloat(arg1)
+        n2= parseFloat(arg2)
+
+return n1+n2
+
+}
+function sub(arg1,arg2){
+    n1= parseFloat(arg1)
+        n2= parseFloat(arg2)
+
+return n1-n2
+
+}
