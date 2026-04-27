fb41f314bdfab9952995a4421813a046b4310a2d
herrabba <hmzaeer@gmail.com>
2026-04-27 13:12:22 +0100

    use infinity

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,5 +1,5 @@
-const biggie=Number.MAX_SAFE_INTEGER
-const smalls=Number.MIN_SAFE_INTEGER
+const biggie=Number.POSITIVE_INFINITY
+const smalls=Number.NEGATIVE_INFINITY
 // console.log(biggie,smalls)
 // console.log(biggie>1.7976931348623157e308)
 
