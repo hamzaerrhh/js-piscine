0badf31fc1f25ceb6ffc9b309cb3fb1c6b66b41d
herrabba <hmzaeer@gmail.com>
2026-04-27 11:48:00 +0100

    greet quize

---
+++ dog-years.js
@@ -0,0 +1,23 @@
+const orbitalPeriods = new Map([
+  ["earth", 1.0],
+  ["mercury", 0.2408467],
+  ["venus", 0.61519726],
+  ["mars", 1.8808158],
+  ["jupiter", 11.862615],
+  ["saturn", 29.447498],
+  ["uranus", 84.016846],
+  ["neptune", 164.79132]
+]);
+
+
+function dogYears(planet, seconds) {
+  const earthYears = seconds / 31557600;
+
+  const planetYears = earthYears / orbitalPeriods.get(planet);
+
+  const result = planetYears * 7;
+
+  return Number(result.toFixed(2));
+}
+
+// console.log(dogYears("earth",1000000000))
