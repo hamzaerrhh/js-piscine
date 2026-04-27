c938e717a3556cc3879212c684cfa3b55c937de9
herrabba <hmzaeer@gmail.com>
2026-04-27 10:02:03 +0100

    varibable declaration

---
+++ primitives.js
@@ -0,0 +1,10 @@
+const str ="1"
+const num = 14
+const bool =true
+const undef = undefined
+
+//log the type of vars
+// console.log(typeof str)
+// console.log(typeof num)
+// console.log(typeof bool)
+// console.log(typeof undef)
