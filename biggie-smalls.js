06822c45250893ca4c714c8f6c3480ba8766dc12
herrabba <hmzaeer@gmail.com>
2026-04-27 12:06:02 +0100

    safety first

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,3 +1,5 @@
-const biggie=Number.MAX_VALUE
-const smalls=Number.MIN_VALUE
-// console.log(biggie,smalls);
+const biggie=Number.MAX_SAFE_INTEGER
+const smalls=Number.MIN_SAFE_INTEGER
+// console.log(biggie,smalls)
+// console.log(biggie>1.7976931348623157e308)
+
