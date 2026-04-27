d535962223f7cc011927137a6ab5e3712ad442e4
herrabba <hmzaeer@gmail.com>
2026-04-27 13:43:41 +0100

    add func samSign

--- sign.js
+++ sign.js
@@ -2,6 +2,15 @@ function sign(n){
     if (n==0){
         return 0
     }
-    return n>0 ? 1:-1
+
+    return n > 0  ? 1 : -1
 }
-console.log(sign(0),sign(10),sign(-6));
+
+function sameSign(n1,n2){
+    return sign(n1)==sign(n2)
+}
+// console.log(sign(0),sign(10),sign(-6));
+
+// console.log(sameSign(4,0));
+// console.log(sameSign(4,7));
+// console.log(sameSign(4,-6));
