5a2eb62f75a8284f42907951824c046421754626
herrabba <hmzaeer@gmail.com>
2026-04-27 11:20:31 +0100

    last-first-kiss

---
+++ last-first-kiss.js
@@ -0,0 +1,20 @@
+function first(arg){
+    return arg[0]
+}
+
+function last(arg){
+    return arg[arg.length-1]
+}
+
+function kiss(arg){
+    return [last(arg),first(arg)]
+}
+// const str = "lolopmaoaloo"
+// const videStr=""
+// const arr =[4,858,'ljk']
+// const arrVide=[]
+
+// console.log(kiss(str));
+// console.log(kiss(videStr));
+// console.log(kiss(arr));
+// console.log(kiss(arrVide));
