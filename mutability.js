53f422877802c3c8aaac2fc010eb81214406733e
herrabba <hmzaeer@gmail.com>
2026-04-27 10:45:01 +0100

    set and get + ref type

--- mutability.js
+++ mutability.js
@@ -9,7 +9,7 @@ const samePerson =person
 person.age++;
 person.country="FR"
 
-console.log("person:",person)
-console.log("clone1:",clone1)
-console.log("clone2:",clone2)
-console.log("samePerson:",samePerson)
+// console.log("person:",person)
+// console.log("clone1:",clone1)
+// console.log("clone2:",clone2)
+// console.log("samePerson:",samePerson)
