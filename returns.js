0f54572b5bfa1f3ae4eecae342420b404b054b94
herrabba <hmzaeer@gmail.com>
2026-04-27 11:13:47 +0100

    returns

--- returns.js
+++ returns.js
@@ -2,7 +2,8 @@ function id(n){
     return n
 }
 function getLength(arr){
-    return nil
+    return arr.length
 }
-let arr = [4,5,2,6,9]
-console.log(arr.length);
+// let arr = [4,5,2,6,9]
+// const str="slslslslslsl"
+// console.log(arr.length,str.length);
