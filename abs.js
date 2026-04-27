767584d8f82cadf8967d847cf9a38f5b8601fffb
herrabba <hmzaeer@gmail.com>
2026-04-27 13:31:28 +0100

    abs

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(n){
+    n = parseFloat(n)
+    if (n >0){
+        return true
+    }
+}
+
+function abs(n){
+    n = parseFloat(n)
+    if (n >0){
+        return n
+    }
+    return -n
+    
+}
