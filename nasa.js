e6cd4253283cdb02ff09a325412362c76f3f4d8c
herrabba <hmzaeer@gmail.com>
2026-04-29 16:23:10 +0100

    nasa

---
+++ nasa.js
@@ -0,0 +1,21 @@
+const nasa = (n) => {
+  sol = "";
+  for (let i = 1; i < n; i++) {
+    if (i % 3 == 0) {
+      sol += "NA";
+    }
+    if (i % 5 == 0) {
+      sol += "SA";
+    }
+    if (i % 5 != 0 && i % 3 != 0) {
+      sol += i;
+    }
+
+    if (i != n - 1) {
+      sol += " ";
+    }
+  }
+  return sol;
+};
+
+// console.log(nasa(16));
