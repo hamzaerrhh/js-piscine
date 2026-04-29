5becd46699f8d48410b909ede9d6d2848d211dda
herrabba <hmzaeer@gmail.com>
2026-04-29 10:13:17 +0100

    elementry

---
+++ elementary.js
@@ -0,0 +1,32 @@
+ const multiply =(a,b)=>{
+       let result=0
+   while(b>0){
+     result +=a
+    b--
+   }
+return result
+ }
+  const divide =(a,b)=>{
+    if (a==b) {return 1}
+       let result=0
+   while(a>0){
+     result++
+       a-=b
+   }
+return result-1
+ }
+
+const modulo =(a,b)=>{
+const divider= divide(a,b)
+const multy=multiply(divider,b)
+return a-multy
+ }
+ 
+
+
+//  console.log(multiply(4,4));
+//   console.log(divide(8,3));
+//     console.log(modulo(8,3));
+
+
+ 
