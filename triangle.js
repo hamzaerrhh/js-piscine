021f4e2417e8415f1f5344e29caeaeedaab47903
herrabba <hmzaeer@gmail.com>
2026-04-29 17:32:27 +0100

    triangle

--- triangle.js
+++ triangle.js
@@ -2,7 +2,7 @@ const triangle = (str, nbr) => {
   let sol = "";
   const lenStr = str.length;
 
-  for (i = 0; i <= nbr; i++) {
+  for (let i = 0; i <= nbr; i++) {
     for (let len = 0; len < i; len++) {
       sol += str;
     }
