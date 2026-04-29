fcb1d4d48877f42bb6ea3f53a30a9a23edae05c7
herrabba <hmzaeer@gmail.com>
2026-04-29 15:16:55 +0100

    ss

--- unbreakable.js
+++ unbreakable.js
@@ -8,25 +8,40 @@ const join = (arr, sep = ",") => {
   }
   return sol;
 };
-const split = (str, sep) => {
-  if (sep === undefined) return [str];
-
-  if (sep === "") return str.split(""); 
-
-  let sol = [];
-  let elem = "";
-  const lenSep = sep.length;
-
-  for (let i = 0; i < str.length; i++) {
-    if (str.slice(i, i + lenSep) === sep) {
-      sol.push(elem);
-      elem = "";
-      i += lenSep - 1;
-    } else {
-      elem += str[i];
+const split = (arr, sep) => {
+  var sol = [];
+  if (sep === undefined) {
+    sol.push(arr);
+  } else {
+    if (sep == "") {
+      for (let i of arr) {
+        sol.push(i);
+      }
+      return sol;
     }
+
+    let elem = "";
+    const lenSep = sep.length;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr.slice(i, i + lenSep) == sep) {
+        sol.push(elem);
+
+        elem = "";
+        i = i + lenSep - 1;
+      } else {
+        elem += arr[i];
+      }
+    }
+    sol.push(elem);
   }
 
-  sol.push(elem);
   return sol;
 };
+
+// const elements = ["Fire", "Air", "Water"];
+// console.log(join(elements, " "));
+// //spplit
+// const str = "The   quick   brown   fox   jumps   over   the   lazy dog.";
+console.log(split("rrrr", "rr"));
+console.log(split("Riad", ""));
