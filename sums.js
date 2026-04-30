59cee2ed8a56a75f70efc89991ba750f75b6996c
herrabba <hmzaeer@gmail.com>
2026-04-30 11:34:23 +0100

    sums

---
+++ sums.js
@@ -0,0 +1,18 @@
+const sums = (n) => {
+  let result = [];
+
+  const backTrack = (start, end, curr) => {
+    if (end === 0) {
+      result.push([...curr]);
+      return;
+    }
+    for (let i = start; i <= end; i++) {
+      curr.push(i);
+      backTrack(i, end - i, curr);
+      curr.pop();
+    }
+  };
+  backTrack(1, n, []);
+  result.pop();
+  return result;
+};
