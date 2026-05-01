c3854b3dae060a5eeac92c0f2e567ede1050b5eb
herrabba <hmzaeer@gmail.com>
2026-05-01 11:04:49 +0100

    inout

---
+++ ion-out.js
@@ -0,0 +1,13 @@
+const ionOut = (str) => {
+  const regex = /\w*tion\w*/g;
+  const reg2 = /tion/g;
+  const matchs = str.match(regex);
+  if (matchs == undefined) {
+    return [];
+  }
+  return matchs.map((s) => s.replace(reg2, "t"));
+};
+
+// const data = "attention, direction";
+
+// console.log(ionOut(data));
--- its-a-match.js
+++ its-a-match.js
@@ -1,5 +1,13 @@
 const normal = /hi/;
+
 const begin = /^hi/;
 const end = /hi$/;
 const beginEnd = /^hi$/;
+
+
+
+
+
+
+
 console.log(end.test("hi"));
