95926678b62a498a6b9b11e68b4504ca5011c9c1
herrabba <hmzaeer@gmail.com>
2026-05-04 15:36:37 +0100

    dr-strange

---
+++ dr-strange.js
@@ -0,0 +1,37 @@
+function addWeek(date) {
+  const epoch = new Date("0001-01-01");
+
+  const msPerDay = 86400000;
+
+  const days = Math.floor((date.getTime() - epoch.getTime()) / msPerDay);
+
+  const dayIndex = ((days % 14) + 14) % 14;
+
+  const week = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "secondMonday",
+    "secondTuesday",
+    "secondWednesday",
+    "secondThursday",
+    "secondFriday",
+    "secondSaturday",
+    "secondSunday",
+  ];
+
+  return week[dayIndex];
+}
+function timeTravel({ date, hour, minute, second }) {
+  const newDate = new Date(date);
+
+  newDate.setHours(hour);
+  newDate.setMinutes(minute);
+  newDate.setSeconds(second);
+
+  return newDate;
+}
