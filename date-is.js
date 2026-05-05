c636eff6b2a2b16f14eea593cc68b16ee050e77d
herrabba <hmzaeer@gmail.com>
2026-05-05 15:35:09 +0100

    qs

--- date-is.js
+++ date-is.js
@@ -1,24 +1,34 @@
-// Checks whether a Date is valid
 function isValid(date) {
-  return date instanceof Date && !isNaN(date.getTime());
+  if (date instanceof Date) {
+    return !Number.isNaN(date.getTime());
+  }
+  if (typeof date === "number") {
+    return !Number.isNaN(new Date(date).getTime());
+  }
+  return false;
+}
+
+function toTime(date) {
+  if (date instanceof Date) return date.getTime();
+  return new Date(date).getTime();
 }
 
 function isAfter(date1, date2) {
   if (!isValid(date1) || !isValid(date2)) return false;
-  return date1.getTime() > date2.getTime();
+  return toTime(date1) > toTime(date2);
 }
 
 function isBefore(date1, date2) {
   if (!isValid(date1) || !isValid(date2)) return false;
-  return date1.getTime() < date2.getTime();
+  return toTime(date1) < toTime(date2);
 }
 
 function isFuture(date) {
   if (!isValid(date)) return false;
-  return date.getTime() > Date.now();
+  return toTime(date) > Date.now();
 }
 
 function isPast(date) {
   if (!isValid(date)) return false;
-  return date.getTime() < Date.now();
+  return toTime(date) < Date.now();
 }
