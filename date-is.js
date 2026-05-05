c71dff67f197c65c4ca650d142decb4a92fb77a1
herrabba <hmzaeer@gmail.com>
2026-05-05 16:07:09 +0100

    change structure

--- date-is.js
+++ date-is.js
@@ -2,12 +2,13 @@ function isValid(date) {
   if (date instanceof Date) {
     return !Number.isNaN(date.getTime());
   }
+
   if (typeof date === "number") {
     return !Number.isNaN(new Date(date).getTime());
   }
   return false;
 }
-
+console.log();
 function toTime(date) {
   if (date instanceof Date) return date.getTime();
   return new Date(date).getTime();
