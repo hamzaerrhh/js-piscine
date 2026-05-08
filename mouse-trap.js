a5f924c8b66230cdb625a37a6ecf2889787dad26
herrabba <hmzaeer@gmail.com>
2026-05-08 14:15:02 +0100

    check in creation

--- mouse-trap.js
+++ mouse-trap.js
@@ -43,29 +43,32 @@ export const createCircle = () => {
   });
 };
 
-export const createCircle = () => {
-  document.addEventListener("click", (e) => {
-    const c = document.createElement("div");
-    c.className = "circle";
+export const moveCircle = () => {
+  document.addEventListener("mousemove", (e) => {
+    const list = document.querySelectorAll(".circle");
+    if (!list.length || !boxDimt) return;
+
+    const c = list[list.length - 1];
+
+    const b = getBox();
 
     let x = e.clientX - R;
     let y = e.clientY - R;
 
-    c.dataset.trapped = "0";
-    c.style.background = "white";
+    const inside = isInside(x, y, b);
 
-    if (boxDimt) {
-      const inside = isInside(x, y, getBox());
-      if (inside) {
-        c.dataset.trapped = "1";
-        c.style.background = "var(--purple)";
-      }
+    if (inside || c.dataset.trapped === "1") {
+      c.style.background = "var(--purple)";
+      c.dataset.trapped = "1";
+    }
+
+    if (c.dataset.trapped === "1") {
+      x = Math.max(b.l, Math.min(x, b.r - SIZE));
+      y = Math.max(b.t, Math.min(y, b.b - SIZE));
     }
 
     c.style.left = `${x}px`;
     c.style.top = `${y}px`;
-
-    document.body.appendChild(c);
   });
 };
 
