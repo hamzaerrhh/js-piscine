89b1ab45f0733359b392b99f661514237b281060
herrabba <hmzaeer@gmail.com>
2026-05-08 14:00:15 +0100

    d

--- mouse-trap.js
+++ mouse-trap.js
@@ -18,52 +18,52 @@ export const createCircle = () => {
 };
 
 export const moveCircle = () => {
+
   document.addEventListener("mousemove", (event) => {
     const circles = document.querySelectorAll(".circle");
     if (circles.length === 0 || !boxDimt) return;
 
     const lastCircle = circles[circles.length - 1];
 
-    let x = event.clientX - CIRCLE_SIZE / 2;
-    let y = event.clientY - CIRCLE_SIZE / 2;
+    const R = 25;
 
-    // circle boundaries
-    const circleDimt = {
-      left: x,
-      top: y,
-      right: x + CIRCLE_SIZE,
-      bottom: y + CIRCLE_SIZE,
-    };
+    let x = event.clientX - R;
+    let y = event.clientY - R;
 
     const safeBox = {
-      left: boxDimt.left + BOX_BORDER,
-      top: boxDimt.top + BOX_BORDER,
-      right: boxDimt.right - BOX_BORDER,
-      bottom: boxDimt.bottom - BOX_BORDER,
+      left: boxDimt.left + 1,
+      top: boxDimt.top + 1,
+      right: boxDimt.right - 1,
+      bottom: boxDimt.bottom - 1,
     };
 
-    const isFullyInside =
-      circleDimt.left >= safeBox.left &&
-      circleDimt.right <= safeBox.right &&
-      circleDimt.top >= safeBox.top &&
-      circleDimt.bottom <= safeBox.bottom;
+    const circle = {
+      left: x,
+      top: y,
+      right: x + 50,
+      bottom: y + 50,
+    };
 
-    // trap logic
-    if (isFullyInside) {
+    const fullyInside =
+      circle.left >= safeBox.left &&
+      circle.right <= safeBox.right &&
+      circle.top >= safeBox.top &&
+      circle.bottom <= safeBox.bottom;
+
+    if (fullyInside || lastCircle.dataset.trapped === "true") {
       lastCircle.style.background = "var(--purple)";
       lastCircle.dataset.trapped = "true";
     }
 
     if (lastCircle.dataset.trapped === "true") {
-      x = Math.max(safeBox.left, Math.min(x, safeBox.right - CIRCLE_SIZE));
-      y = Math.max(safeBox.top, Math.min(y, safeBox.bottom - CIRCLE_SIZE));
+      x = Math.max(safeBox.left, Math.min(x, safeBox.right - 50));
+      y = Math.max(safeBox.top, Math.min(y, safeBox.bottom - 50));
     }
 
     lastCircle.style.left = `${x}px`;
     lastCircle.style.top = `${y}px`;
   });
 };
-
 export const setBox = () => {
   const box = document.createElement("div");
   box.classList.add("box");
