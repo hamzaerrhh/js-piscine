a196d8d0ad16e63ffbe749ee1270d2cf74d44e1b
herrabba <hmzaeer@gmail.com>
2026-05-18 17:00:51 +0100

    test

--- happiness-manager.mjs
+++ happiness-manager.mjs
@@ -1,4 +1,4 @@
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 const [, , guestDir, outputFile] = process.argv
 
@@ -7,20 +7,28 @@ if (!guestDir || !outputFile) {
   process.exit(1)
 }
 
-fs.readdir(guestDir, (err, files) => {
-  if (err) {
-    console.log('error reading dir')
-    process.exit(1)
-  }
+let files
 
-  let drinkCounts = {
-    'iced-tea': 0,
-    water: 0,
-    'sparkling-water': 0,
-    soft: 0,
-  }
+try {
+  files = await fs.readdir(guestDir)
+} catch {
+  console.log('error reading dir')
+  process.exit(1)
+}
 
- let foodCounts = {
+if (files.length === 0) {
+  console.log('No one is coming.')
+  process.exit(0)
+}
+
+const drinkCounts = {
+  'iced-tea': 0,
+  water: 0,
+  'sparkling-water': 0,
+  soft: 0,
+}
+
+const foodCounts = {
   veggie: 0,
   vegan: 0,
   carnivore: 0,
@@ -28,134 +36,102 @@ fs.readdir(guestDir, (err, files) => {
   everything: 0,
 }
 
-  let vipCount = 0
-  let pendingFiles = files.length
+let vipCount = 0
 
-  // empty folder
-  if (pendingFiles === 0) {
-    console.log('No one is coming.')
-    process.exit(0)
-  }
+for (const file of files) {
+  try {
+    const data = await fs.readFile(`${guestDir}/${file}`, 'utf-8')
 
-  for (const file of files) {
-    fs.readFile(`${guestDir}/${file}`, 'utf-8', (err, data) => {
-      pendingFiles--
+    const guest = JSON.parse(data)
 
-      if (!err) {
-        const guest = JSON.parse(data)
+    if (guest.answer === 'yes') {
+      vipCount++
 
-        if (guest.answer === 'yes') {
-          vipCount++
-
-          if (guest.drink) {
-            drinkCounts[guest.drink]++
-          }
-
-          if (guest.food) {
-            foodCounts[guest.food]++
-          }
-        }
+      if (guest.drink) {
+        drinkCounts[guest.drink]++
       }
 
-      // wait until all files are processed
-      if (pendingFiles === 0) {
-        finish()
+      if (guest.food) {
+        foodCounts[guest.food]++
       }
-    })
+    }
+
+  } catch {
   }
+}
 
-  function finish() {
-    if (vipCount === 0) {
-      console.log('No one is coming.')
-      process.exit(0)
-    }
+if (vipCount === 0) {
+  console.log('No one is coming.')
+  process.exit(0)
+}
 
-    let shoppingList = {}
+const shoppingList = {}
 
-    // DRINKS
 
-    if (drinkCounts['iced-tea'] > 0) {
-      shoppingList['iced-tea-bottles'] = Math.ceil(
-        drinkCounts['iced-tea'] / 6
-      )
-    }
+if (drinkCounts['iced-tea'] > 0) {
+  shoppingList['iced-tea-bottles'] =
+    Math.ceil(drinkCounts['iced-tea'] / 6)
+}
 
-    if (drinkCounts.water > 0) {
-      shoppingList['water-bottles'] = Math.ceil(
-        drinkCounts.water / 4
-      )
-    }
+if (drinkCounts.water > 0) {
+  shoppingList['water-bottles'] =
+    Math.ceil(drinkCounts.water / 4)
+}
 
-    if (drinkCounts['sparkling-water'] > 0) {
-      shoppingList['sparkling-water-bottles'] = Math.ceil(
-        drinkCounts['sparkling-water'] / 4
-      )
-    }
+if (drinkCounts['sparkling-water'] > 0) {
+  shoppingList['sparkling-water-bottles'] =
+    Math.ceil(drinkCounts['sparkling-water'] / 4)
+}
 
-    if (drinkCounts.soft > 0) {
-      shoppingList['soft-bottles'] = Math.ceil(
-        drinkCounts.soft / 4
-      )
-    }
+if (drinkCounts.soft > 0) {
+  shoppingList['soft-bottles'] =
+    Math.ceil(drinkCounts.soft / 4)
+}
 
-    // FOOD
 
-    const veggieTotal =
-      foodCounts.veggie + foodCounts.vegan
 
-    if (veggieTotal > 0) {
-      const packs = Math.ceil(veggieTotal / 3)
+const veggieTotal =
+  foodCounts.veggie + foodCounts.vegan
 
-      shoppingList.eggplants = packs
-      shoppingList.courgettes = packs
-shoppingList.mushrooms = veggieTotal  
-    shoppingList.hummus = packs
-    }
+if (veggieTotal > 0) {
+  const packs = Math.ceil(veggieTotal / 3)
 
-    if (foodCounts.carnivore > 0) {
-      shoppingList.burgers = foodCounts.carnivore
-    }
+  shoppingList.eggplants = packs
+  shoppingList.courgettes = packs
+  shoppingList.mushrooms = veggieTotal
+  shoppingList.hummus = packs
+}
 
-    if (foodCounts.fish > 0) {
-      shoppingList.sardines = foodCounts.fish
-    }
+if (foodCounts.carnivore > 0) {
+  shoppingList.burgers = foodCounts.carnivore
+}
 
- if (foodCounts.everything > 0) {
+if (foodCounts.fish > 0) {
+  shoppingList.sardines = foodCounts.fish
+}
+
+if (foodCounts.everything > 0) {
   shoppingList.kebabs = foodCounts.everything
 }
 
-    shoppingList.potatoes = vipCount
+shoppingList.potatoes = vipCount
 
-    // check if output file exists
-    fs.stat(outputFile, (err) => {
-      if (err) {
-        // file doesn't exist
-        fs.writeFile(
-          outputFile,
-          JSON.stringify(shoppingList, null, 2),
-          () => {}
-        )
-      } else {
-        // file exists
-        fs.readFile(outputFile, 'utf-8', (err, oldData) => {
-          let existingData = {}
+let existingData = {}
 
-          if (!err) {
-            existingData = JSON.parse(oldData)
-          }
+try {
+  const oldData = await fs.readFile(outputFile, 'utf-8')
+  existingData = JSON.parse(oldData)
+} catch {
+}
 
-          // merge manually
-          for (const key in shoppingList) {
-            existingData[key] = shoppingList[key]
-          }
+const finalData = {
+  ...existingData,
+  ...shoppingList,
+}
 
-          fs.writeFile(
-            outputFile,
-            JSON.stringify(existingData, null, 2),
-            () => {}
-          )
-        })
-      }
-    })
-  }
-})
+
+await fs.writeFile(
+  outputFile,
+  JSON.stringify(finalData,)
+)
+
